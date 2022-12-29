5ea3755adc1fc8cf9cc86e11f8dfa1dfbe3dccb1
Tatiana <tatiana@example.com>
2022-12-29 14:03:17 +1000

    program

--- final/Program.cs
+++ final/Program.cs
@@ -2,27 +2,25 @@
 // длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-string[] array1 = new string[4] {"27", "2", "354", "17"};
-string[] array2 = new string[array1.Length];
-void SecondArrayWithIF(string[] array1, string[] array2)
+using System;
+ 
+string[] array =
 {
-    int count = 0;
-    for (int i = 0; i < array1.Length; i++)
+    "a",
+    "bb",
+    "ccc",
+    "dddd"   
+   };
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
     {
-    if(array1[i].Length <= 3)
-        {
-        array2[count] = array1[i];
-        count++;
-        }
+        result[realSize] = value;
+        realSize++;
     }
 }
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-SecondArrayWithIF(array1, array2);
-PrintArray(array2);
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
